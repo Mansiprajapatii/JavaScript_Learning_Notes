7ed1c717b0b9c4a1a3365451adc7a785cda91c79
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-29 15:26:24 +0530

    added promise api

--- learn_js_through_code_by_mansi.js
+++ learn_js_through_code_by_mansi.js
@@ -1052,7 +1052,7 @@
 // 1. Promise.all -  it take the array of promises as input and return array of output and if there is failure in any of the promise it will immediately through the error of that promise
 
 const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("p1 successful"), 3000);
+  setTimeout(() => reject("p1 successful"), 3000);
 });
 
 const p2 = new Promise((resolve, reject) => {
@@ -1090,10 +1090,11 @@ const p3 = new Promise((resolve, reject) => {
 //   });
 
 // 4. Promise.any =  similar to race but it will wait for the 1st successful promise and return the value of the success promise
-// Promise.any([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+Promise.any([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+    console.log(err.errors);
+  });
